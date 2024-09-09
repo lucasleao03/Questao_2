94a3183be5dcbc10252cfd9c25f5f7f7c8cd2b01
lucasleao03 <153466619+lucasleao03@users.noreply.github.com>
2024-09-09 16:35:55 -0300

    Add files via upload

---
+++ caNum_complexo/caNum_complexo/Num_complexo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caNum_complexo
+{
+    internal class Num_complexo
+    {
+        //atributos
+        private double re;  //Real
+        private double im;  //Imaginário
+
+        //Construtores
+        public Num_complexo()
+        {
+            re = 0;
+            im = 0;
+        }
+
+        public Num_complexo(double _re, double _im)
+        {
+            re = _re;
+            im = _im;
+        }
+
+        //getters e setters
+        public double Re { get => re; set => re = value; }
+        public double Im { get => im; set => im = value; }
+
+        //Métodos
+        public Num_complexo soma(Num_complexo y)
+        {
+            Num_complexo res = new Num_complexo();
+            res.re = (this.re + y.re);
+            res.im = (this.im + y.im);
+            return res;
+        }
+
+        public Num_complexo vezes(Num_complexo y)
+        {
+            Num_complexo res = new Num_complexo();
+            res.re = (this.re * y.re) + ((this.im * y.im) * (-1));
+            res.im = (this.re * y.im) + (this.im * y.re);
+            return res;
+        }
+
+        public double Modulo()
+        {
+            double x = (this.re) * (this.re);
+            double y = (this.im) * (this.im);
+            return (Math.Sqrt(x + y));
+        }
+
+        public double argumento()
+        {
+            double res = (im / re);
+            return (Math.Atan(res));
+        }
+
+        public void ImprimeFormaPolar()
+        {
+            double ro = this.Modulo();
+            double cos = ((re) / (ro));
+            double sin = ((im) / (ro));
+            double P = (ro * (cos + sin));
+
+            Console.WriteLine("P = " + P);
+
+        }
+    }
+}
---
+++ caNum_complexo/caNum_complexo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caNum_complexo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Num_complexo z1 = new Num_complexo(1, 1);
+            Num_complexo z2 = new Num_complexo(3,-1);
+
+            Num_complexo z3 = new Num_complexo();
+            z3 = z1.soma(z2);
+            z3.ImprimeFormaPolar();
+
+            Console.WriteLine(z3.Re);
+            Console.WriteLine(z3.Im);
+
+            Num_complexo z4 = new Num_complexo();
+            z4 = z1.vezes(z2);
+
+            Console.WriteLine(z4.Re);
+            Console.WriteLine(z4.Im);
+
+            Num_complexo z5 = new Num_complexo(3, 4);
+            Console.WriteLine(z5.Modulo());
+            z5.ImprimeFormaPolar();
+
+            Console.ReadLine();
+        }
+    }
+}
